298ced35b745743b025ed8d8020090aca728ffa5
AnnaPrints <65970510+AnnaPrints@users.noreply.github.com>
2024-03-11 08:39:45 -0700

    Add files via upload

---
+++ target/debug/deps/test_folding_in_tests-df6dc6f814bd05f8.d
@@ -0,0 +1,5 @@
+/Users/jetbrains/CLionProjects/test_folding_in_tests/target/debug/deps/test_folding_in_tests-df6dc6f814bd05f8: src/lib.rs
+
+/Users/jetbrains/CLionProjects/test_folding_in_tests/target/debug/deps/test_folding_in_tests-df6dc6f814bd05f8.d: src/lib.rs
+
+src/lib.rs:
